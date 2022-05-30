b62dd082c06339b05fdbeb357e68c19742ea0c93
Karthik Rana <karrana1@WKMZT7C17FFD.global.publicisgroupe.net>
2022-05-30 12:39:12 +0530

    Firebase update

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -8,12 +8,12 @@ import {
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyAcRdTnwvRtYZYptStH_kaZcJV3aHV1B4c",
+  authDomain: "crwn-clothing-db-3fc6b.firebaseapp.com",
+  projectId: "crwn-clothing-db-3fc6b",
+  storageBucket: "crwn-clothing-db-3fc6b.appspot.com",
+  messagingSenderId: "1062717763080",
+  appId: "1:1062717763080:web:6375982e72009e44bd6bbd"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
